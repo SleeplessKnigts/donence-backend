1b8b6770e18d29c54e5e7e8cc4517bd7829b0bf9
Kerem <krmtrk20@gmail.com>
2021-01-12 14:15:10 +0300

    add some delicate configuration
    Most of them is for configuring oauth2 classes

---
+++ src/main/java/com/donence/config/WebSecurityConfig.java
@@ -0,0 +1,70 @@
+package com.donence.config;
+
+import com.donence.security.AuthEntryPoint;
+import com.donence.security.TokenAuthenticationFilter;
+import com.donence.security.oauth2.HttpCookieOAuth2AuthorizationRequestRepository;
+import com.donence.security.oauth2.OAuth2AuthenticationFailureHandler;
+import com.donence.security.oauth2.OAuth2AuthenticationSuccessHandler;
+import com.donence.security.oauth2.OAuth2UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import java.lang.Override;
+
+@EnableWebSecurity
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private OAuth2UserService oAuth2UserService;
+
+    @Autowired
+    private AuthEntryPoint unauthorizedHandler;
+
+    @Autowired
+    private OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler;
+
+    @Autowired
+    private OAuth2AuthenticationFailureHandler oAuth2AuthenticationFailureHandler;
+
+    @Bean
+    public TokenAuthenticationFilter tokenAuthenticationFilter() {
+        return new TokenAuthenticationFilter();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and().authorizeRequests()
+                .antMatchers("/", "/error", "/favicon.ico", "/**/*.css", "/**/*.js", "/**/*.html", "/**/*.jpg")
+                .permitAll()
+                .antMatchers("/auth/**", "/oauth2/**").permitAll()
+                .anyRequest().authenticated()
+                .and().oauth2Login()
+                .authorizationEndpoint().baseUri("/api/oauth2/authorize")
+                .authorizationRequestRepository(cookieAuthorizationRequestRepository()).and().redirectionEndpoint()
+                .baseUri("/oauth2/callback/*").and().userInfoEndpoint().userService(oAuth2UserService)
+                .and().successHandler(oAuth2AuthenticationSuccessHandler)
+                .failureHandler(oAuth2AuthenticationFailureHandler);
+        http.addFilterBefore(tokenAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Bean
+    public HttpCookieOAuth2AuthorizationRequestRepository cookieAuthorizationRequestRepository() {
+        return new HttpCookieOAuth2AuthorizationRequestRepository();
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+}
