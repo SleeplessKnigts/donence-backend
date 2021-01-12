ff34911745203c6712d4a02769effe32de401b48
Kerem <krmtrk20@gmail.com>
2021-01-12 14:18:28 +0300

    add oauth2 authentication classes
    some reworks may be needed(throwing a proper exception etc.)

---
+++ src/main/java/com/donence/security/oauth2/OAuth2UserService.java
@@ -0,0 +1,70 @@
+package com.donence.security.oauth2;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.InternalAuthenticationServiceException;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import java.util.Optional;
+
+import com.donence.model.User;
+import com.donence.repository.UserRepository;
+import com.donence.security.oauth2.user.OAuth2UserInfo;
+import com.donence.security.oauth2.user.OAuth2UserInfoFactory;
+import com.donence.security.services.UserDetailsImpl;
+
+@Service
+public class OAuth2UserService extends DefaultOAuth2UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest oAuth2UserRequest) throws OAuth2AuthenticationException {
+        OAuth2User oAuth2User = super.loadUser(oAuth2UserRequest);
+
+        try {
+            return processOAuth2User(oAuth2UserRequest, oAuth2User);
+        } catch (AuthenticationException ex) {
+            throw ex;
+        } catch (Exception ex) {
+            throw new InternalAuthenticationServiceException(ex.getMessage(), ex.getCause());
+        }
+    }
+
+    private OAuth2User processOAuth2User(OAuth2UserRequest oAuth2UserRequest, OAuth2User oAuth2User)
+            throws javax.security.sasl.AuthenticationException {
+        OAuth2UserInfo oAuth2UserInfo = OAuth2UserInfoFactory.getOAuth2UserInfo(
+                oAuth2UserRequest.getClientRegistration().getRegistrationId(), oAuth2User.getAttributes());
+
+        if (!StringUtils.hasText(oAuth2UserInfo.getEmail())) {
+            throw new RuntimeException("Email not found from OAuth2 provider");
+        }
+
+        Optional<User> userOptional = userRepository.findByEmail(oAuth2UserInfo.getEmail());
+        User user;
+        if (userOptional.isPresent()) {
+            user = userOptional.get();
+        } else {
+            user = registerNewUser(oAuth2UserRequest, oAuth2UserInfo);
+        }
+
+        return UserDetailsImpl.build(user, oAuth2User.getAttributes());
+    }
+
+    private User registerNewUser(OAuth2UserRequest oAuth2UserRequest, OAuth2UserInfo oAuth2UserInfo) {
+        System.out.println("Hello");
+        User user = new User();
+        user.setAuthProvider(oAuth2UserRequest.getClientRegistration().getRegistrationId());
+        user.setFName(oAuth2UserInfo.getName());
+        user.setEmail(oAuth2UserInfo.getEmail());
+        user.setImageUrl(oAuth2UserInfo.getImageUrl());
+        System.out.println(user);
+        return userRepository.save(user);
+    }
+}
