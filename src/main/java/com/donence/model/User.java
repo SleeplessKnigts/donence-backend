cdfd4717a98d0cd4e4bfa1390c46da4e9798613d
Kerem <krmtrk20@gmail.com>
2021-02-26 22:15:36 +0300

    add relationship to requests model

--- src/main/java/com/donence/model/User.java
+++ src/main/java/com/donence/model/User.java
@@ -1,5 +1,9 @@
 package com.donence.model;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -8,9 +12,12 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -52,6 +59,10 @@ public class User {
     @ManyToOne(fetch = FetchType.EAGER, optional = false)
     @JoinColumn(name = "role_id", nullable = false)
     private Role role;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "issuer", cascade = CascadeType.ALL)
+    @JsonManagedReference
+    private Set<Request> requests = new HashSet<>();
     
     public User(String email, String username) {
         this.email = email;
