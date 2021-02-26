b413b4e7bc03f2507e548d3cff50278e037020d2
Kerem <krmtrk20@gmail.com>
2021-02-26 16:55:23 +0300

    add methods for address set and change

--- src/main/java/com/donence/controller/UserController.java
+++ src/main/java/com/donence/controller/UserController.java
@@ -1,5 +1,8 @@
 package com.donence.controller;
 
+import javax.validation.Valid;
+
+import com.donence.dto.request.SetAddressForm;
 import com.donence.model.User;
 import com.donence.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,8 +10,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 @RequestMapping("api/user")
@@ -17,14 +21,28 @@ public class UserController {
     UserService userService;
 
     /**
-     * An endpoint to give address save functionality.
+     * An endpoint for address saving functionality.
      * 
+     * @param SetAdressForm required request body including lat-lon double fields.
      */
     @PostMapping("/address")
-    public ResponseEntity<?> setAddress(){
-        System.out.println("Hello there!");
+    public ResponseEntity<?> setAddress(@Valid @RequestBody SetAddressForm setAddressForm) {
         User user = userService.getUserByAuthentication(SecurityContextHolder.getContext().getAuthentication());
-        return ResponseEntity.ok().body("Address should be returned here");
+        user.setAddress(setAddressForm.getLat(), setAddressForm.getLon());
+        userService.save(user);
+        return ResponseEntity.ok().body(user);
     }
 
+    /**
+     * An endpoint for address saving functionality.
+     * 
+     * @param SetAdressForm required request body including lat-lon double fields.
+     */
+    @PutMapping("/address")
+    public ResponseEntity<?> changeAddress(@Valid @RequestBody SetAddressForm setAddressForm) {
+        User user = userService.getUserByAuthentication(SecurityContextHolder.getContext().getAuthentication());
+        user.setAddress(setAddressForm.getLat(), setAddressForm.getLon());
+        userService.save(user);
+        return ResponseEntity.ok().body(user);
+    }
 }
