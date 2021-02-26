4bfb687cf662255f0820e9bc0cdb417a3a4009ee
Kerem <krmtrk20@gmail.com>
2021-02-26 16:54:27 +0300

    add address request form

---
+++ src/main/java/com/donence/dto/request/SetAddressForm.java
@@ -0,0 +1,23 @@
+package com.donence.dto.request;
+
+import javax.validation.constraints.DecimalMax;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotNull;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+public class SetAddressForm {
+    @DecimalMax("90.0")
+    @DecimalMin("-90.0")
+    @NotNull(message = "lon field cannot be empty")
+    private Double lat;
+
+    @DecimalMax("180.0")
+    @DecimalMin("-180.0")
+    @NotNull(message = "lon field cannot be empty")
+    private Double lon;
+
+}
