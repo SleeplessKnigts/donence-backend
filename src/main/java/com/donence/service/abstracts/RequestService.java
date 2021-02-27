74ff0cf90e4ea455908e77b1a58d7b08bae1dd36
Arif <marfsahin@gmail.com>
2021-02-27 15:21:17 +0300

    add request service

---
+++ src/main/java/com/donence/service/abstracts/RequestService.java
@@ -0,0 +1,14 @@
+package com.donence.service.abstracts;
+
+import com.donence.model.Request;
+import com.donence.model.User;
+
+import java.util.List;
+
+public interface RequestService{
+    List<Request> getRequestsByActiveOrderByCreationDateDesc(Boolean isActive);
+
+    List<Request> getRequestsByActiveAndIssuerOrderByCreationDateDesc(Boolean isActive, User issuer);
+
+    List<Request> getRequestsByIssuerOrderByCreationDateDesc(User issuer);
+}
---
+++ src/main/java/com/donence/service/concretes/RequestServiceImpl.java
@@ -0,0 +1,32 @@
+package com.donence.service.concretes;
+
+import com.donence.model.Request;
+import com.donence.model.User;
+import com.donence.repository.RequestRepository;
+import com.donence.service.abstracts.RequestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class RequestServiceImpl implements RequestService {
+
+    @Autowired
+    RequestRepository requestRepository;
+
+    @Override
+    public List<Request> getRequestsByActiveOrderByCreationDateDesc(Boolean isActive) {
+        return requestRepository.findByIsActiveOrderByCreationDateDesc(isActive);
+    }
+
+    @Override
+    public List<Request> getRequestsByActiveAndIssuerOrderByCreationDateDesc(Boolean isActive, User issuer) {
+        return requestRepository.findByIsActiveAndIssuerOrderByCreationDateDesc(isActive, issuer);
+    }
+
+    @Override
+    public List<Request> getRequestsByIssuerOrderByCreationDateDesc(User issuer) {
+        return requestRepository.findByIssuerOrderByCreationDateDesc(issuer);
+    }
+}
