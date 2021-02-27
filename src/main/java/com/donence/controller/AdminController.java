5ca12ffc2c40dcc1cea53bff70dd08e9038b926b
Arif <marfsahin@gmail.com>
2021-02-27 16:12:49 +0300

    add endpoints for admin to view request infos.

--- src/main/java/com/donence/controller/AdminController.java
+++ src/main/java/com/donence/controller/AdminController.java
@@ -2,32 +2,53 @@ package com.donence.controller;
 
 import com.donence.dto.request.RecyclePointDto;
 import com.donence.model.RecyclePoint;
-import com.donence.service.RecyclePointService;
+import com.donence.model.Request;
+import com.donence.service.abstracts.RecyclePointService;
+import com.donence.service.abstracts.RequestService;
+import com.donence.service.abstracts.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/admin/")
+@RequestMapping("/api/admin")
 public class AdminController {
 
     @Autowired
     RecyclePointService recyclePointService;
 
+    @Autowired
+    RequestService requestService;
+
+    @Autowired
+    UserService userService;
+
     @GetMapping("/recycle-point/")
-    public ResponseEntity<?> getRecyclePoints(){
-        return null;
+    public ResponseEntity<List<RecyclePoint>> getRecyclePoints() {
+        return ResponseEntity.ok(recyclePointService.getRecyclePoints());
     }
 
     @PostMapping("/recycle-point/new")
-    public ResponseEntity<?> addRecyclePoint(@Valid @RequestBody RecyclePointDto recyclePointDto){
+    public ResponseEntity<?> addRecyclePoint(@RequestBody RecyclePointDto recyclePointDto) {
         RecyclePoint recyclePoint = new RecyclePoint();
-        recyclePoint.setRecylePointName(recyclePointDto.getRecyclePointName());
-        recyclePoint.setGeolocation(recyclePointDto.getGeolocation());
+        recyclePoint.setRecyclePointDetail(recyclePointDto.getRecyclePointDetail());
+        recyclePoint.setGeolocation(recyclePointDto.getLatitude(), recyclePointDto.getLongitude());
         recyclePointService.add(recyclePoint);
         return ResponseEntity.ok("Recycle point added successfully");
     }
+
+    @GetMapping(value = {"/requests", "/requests/{isActive}"})
+    public ResponseEntity<List<Request>> getActiveRequests(@PathVariable Optional<Boolean> isActive) {
+        return isActive.map(active -> ResponseEntity.ok(requestService.getRequestsByActiveOrderByCreationDateDesc(active))).orElseGet(() -> ResponseEntity.ok(requestService.getRequestOrderByCreationDateDesc()));
+    }
+
+    @GetMapping(value = {"/requests/get-user-request/{userId}", "/requests/get-user-request/{userId}/{isActive}"})
+    public ResponseEntity<List<Request>> getRequests(@PathVariable Integer userId, @PathVariable Optional<Boolean> isActive) {
+        return isActive.map(active -> ResponseEntity.ok(requestService.getRequestsByActiveAndIssuerOrderByCreationDateDesc(isActive.get(), userService.getUserById(userId)))).orElseGet(() -> ResponseEntity.ok(requestService.getRequestsByIssuerOrderByCreationDateDesc(userService.getUserById(userId))));
+    }
+
 
 }
