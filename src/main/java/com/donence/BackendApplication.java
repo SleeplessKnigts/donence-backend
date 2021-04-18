05de7d04f0fccb290d4ca5e6c24714c97f356577
Kerem <krmtrk20@gmail.com>
2021-04-18 15:12:14 +0300

    fix: frontend urls

--- src/main/java/com/donence/BackendApplication.java
+++ src/main/java/com/donence/BackendApplication.java
@@ -45,7 +45,7 @@ public class BackendApplication {
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         final CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+        config.setAllowedOrigins(Collections.singletonList("https://donence.web.app"));
         config.setAllowedHeaders(Arrays.asList("*"));
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH"));
         source.registerCorsConfiguration("/**", config);
