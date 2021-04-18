537ac07208cbec7e1bc03affe5bacb86c89c947e
Kerem <krmtrk20@gmail.com>
2021-04-18 16:54:49 +0300

    add data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,3 +7,11 @@ WHERE NOT EXISTS(SELECT * FROM cloud_roles WHERE role = 'ROLE_DRIVER');
 INSERT INTO cloud_roles (role)
 SELECT 'ROLE_USER'
 WHERE NOT EXISTS(SELECT * FROM cloud_roles WHERE role = 'ROLE_USER');
+
+update 'cloud_users'
+set role_id=1
+where id=101;
+
+update 'cloud_users'
+set latitude=39,longitude=32
+where id=102;
