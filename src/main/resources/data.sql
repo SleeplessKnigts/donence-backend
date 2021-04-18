462cd190ec6545a29f603fa89d6f59e672b1c19f
Kerem <krmtrk20@gmail.com>
2021-04-18 17:31:10 +0300

    remove admin user

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,5 +8,5 @@ INSERT INTO cloud_roles (role)
 SELECT 'ROLE_USER'
 WHERE NOT EXISTS(SELECT * FROM cloud_roles WHERE role = 'ROLE_USER');
 
-INSERT INTO cloud_users (id, username, email, auth_provider, latitude, longitude, role_id)
-values (1, 'admin', 'donence.dev@gmail.com', 'google', 32.0, 39.0, 1)
+-- INSERT INTO cloud_users (id, username, email, auth_provider, latitude, longitude, role_id)
+-- values (1, 'admin', 'donence.dev@gmail.com', 'google', 32.0, 39.0, 1)
